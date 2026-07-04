1ff8602516e4c856c34d25e5f420db4ecae68642
KunalBagul303 <kunalbagul315@gmail.com>
2026-07-04 22:16:50 +0530

    new folder

---
+++ 04-card-project/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="https://www.vectorstock.com/royalty-free-vectors/amazon-logo-vectors" alt="" />
+          <button>Save</button>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="bottom">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
